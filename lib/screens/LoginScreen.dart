8911760c12d04a3f984236b9e617b60fd4ca71d9
divyaraj773 <divyaraj2706@gmail.com>
2020-03-06 16:47:14 +0530

    second commit

---
+++ lib/screens/LoginScreen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './HomeScreen.dart';
+
+class LoginScreen extends StatelessWidget{
+  final _phoneController = TextEditingController();
+  final _codeController = TextEditingController();
+
+  Future<bool> loginUser(String phone, BuildContext context) async{
+    FirebaseAuth _auth = FirebaseAuth.instance;
+    _auth.verifyPhoneNumber(
+      phoneNumber: phone, 
+      timeout: Duration(seconds: 60), 
+      verificationCompleted: (AuthCredential credential) async{
+        Navigator.of(context).pop();
+
+        AuthResult result = await _auth.signInWithCredential(credential);
+
+        FirebaseUser user = result.user;
+
+        if(user != null){
+          Navigator.push(context, MaterialPageRoute(
+            builder: (context) => HomeScreen(user: user,) ));
+        } else{
+          print("Error");
+        }
+      }, 
+      verificationFailed: (AuthException exception){
+        print(exception);
+      }, 
+      codeSent: (String verificationId, [int forceResendingToken]){
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context){
+            return AlertDialog(
+              title: Text("Enter Code."),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextField(
+                    controller: _codeController,
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: () async{
+                    final code = _codeController.text.trim();
+                    AuthCredential credential = PhoneAuthProvider.getCredential(verificationId: verificationId, smsCode: code);
+
+                    AuthResult result = await _auth.signInWithCredential(credential);
+
+                    FirebaseUser user = result.user;
+
+                    if(user != null){
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => HomeScreen(user: user,)
+                      ));
+                    }else{
+                      print("Error");
+                    }
+                  }, 
+                  child: Text("Verify"),textColor: Colors.white,color: Colors.blue,),
+              ],
+            );
+          }
+        );
+      }, 
+      codeAutoRetrievalTimeout: null);
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Login", style: TextStyle(color: Colors.lightBlue, fontSize: 36,fontWeight: FontWeight.w500),),
+
+                SizedBox(height: 16,),
+
+                TextFormField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            borderSide: BorderSide(color: Colors.grey[200])
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            borderSide: BorderSide(color: Colors.grey[300])
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[100],
+                        hintText: "Mobile Number"
+                  ),
+                  controller: _phoneController,
+                ),
+
+                SizedBox(height: 16,),
+
+                Container(
+                  width: double.infinity,
+                  child: FlatButton(
+                    onPressed: (){
+                      final phone = _phoneController.text.trim();
+                      loginUser(phone, context);
+                    },color: Colors.blue, 
+                    child: Text("LOGIN"),textColor: Colors.white,padding: EdgeInsets.all(16),),
+                )
+              ],
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
